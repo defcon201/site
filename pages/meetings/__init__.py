80caca15d94e35d0a4021a8e1c4cb419bb607024
SidepocketNeo <sidepocket@protonmail.com>
2019-12-20 04:08:58 +0000

    Update __init__.py
    Update Meeting Details for December 2019

--- pages/meetings/__init__.py
+++ pages/meetings/__init__.py
@@ -2,30 +2,30 @@
 
 class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend against it for semantics sake.
     def Date():			# Date of the meeting (Weekday, Month DD)
-        return "Friday, November 15th"
+        return "Friday, December 20th"
     def Title():		# Title of the meeting
-        return "No NAT November"
+        return "HoHoCon Part Deux"
     def Location():		# Location of the meeting (as of spring 2019, "SUBCULTURE Jersey City, 260 Newark Ave.")
         return "SUBCULTURE Jersey City, 260 Newark Ave."
     def Notice():		# Any important notices about the meeting or the information.
         return ""
     def CanonicalLink():	# A link that refers to this meeting
-        return "https://medium.com/@defcon201/defcon-201-meet-up-november-2019-no-network-address-translation-november-b7a0cdf4a407"
+        return "https://medium.com/@defcon201/defcon-201-meet-up-december-2019-hohocon-part-deux-906ee4a1bee9"
     def LinkName():		# What this link refers to (for instance "post on our Medium", "forum post", "tweet", "Meetup event")
         return "Medium:"
     def Summary():		# Short, 1-2 sentences about this meeting.
         return """
-        Join #DEFCON201 at SubCulture (260 Newark Ave, Jersey City NJ) on Friday, November 15th from 7pm - 10pm to learn about Web Browser Privacy & Security, live booting Windows, PWNagotchi, lockpicking and more!"""
+        Join #DEFCON201 at SubCulture (260 Newark Ave, Jersey City NJ) on Friday, December 20th from 7pm - 10pm to watch Mr. Robot, share gifts, listen to Hacker Rants, Nintendo Labo, justCTF, lockpicking and more!"""
     def Schedule():
         schedstruct = ['starttime', 'endtime', 'eventtype', 'title', 'speaker', 'summary']
         schedtable = []
         schedlist = []
-        schedlist.append(['7:00PM', '8:05PM', 'Intro', 'Meet &amp; Greet', 'DEFCON 201 members', 'Chat amongst friends, coordinate your public key portfolio, add cards to your Rolodex&trade;, and warm up for the talks!'])
-        schedlist.append(['8:00PM', '8:30PM', 'Browsing The Internet Privately & Securely',  'Sidepocket', 'DEFCON 201', 'In this talk, we will help you navigate the confusing world of Web Browser Privacy & Security aimed for everyone but ideally for beginners.'])
-        schedlist.append(['8:30PM', '8:35PM', '<em>Windows When You Need ’em, Walls When You Don’t</em>',  'sirocyl', 'DEFCON 201', 'At MAGFest this year, sirocyl will be doing a workshop on a program called FamiTracker which is Windows-only, doesn’t work in WINE or ReactOS and will need to run on people’s own laptops; including those running Linux or something else.'])
-        schedlist.append(['8:35PM', '9:55PM', 'Workshops + Hackathon', 'June 2019', 'Open participation', 'Talk about and work together on projects! Open Projects Include Lockpicking, PWNagotchi, Live Booting Windows and Nintendo LABO VR Kits!'])
+        schedlist.append(['7:00PM', '7:55PM', 'Workshops', 'Meet &amp; Greet + Mr. Robot Screening', 'Open participation', 'Chat amongst friends and work together on projects, watch Mr. Robot Episode 405 Method Not Allowed, learn lockpicking, compete in justCTF and warm up for the talks!'])
+        schedlist.append(['7:55PM', '8:00PM', 'A Sneak Peak Of DEFCON 201 In 2020',  'Sidepocket & GI Jack', 'DEFCON 201', 'In this talk, join our Co-Founders at a sneak peak into our bright feature and how the Dirty Jersey smell is about to embark it’s foul hacker stank nationwide!'])
+        schedlist.append(['8:00PM', '8:10PM', '<em>Secret Hacker Santa Swap</em>',  'DEFCON 201 Attendees', 'DEFCON 201', 'Bring out your inexpensive ($15 or Under) hacker gifts for our first evenr Secret Hacker Santa Swap! It’s like a White Elephant or Yankie Swap…but with 1337 Haxxors!'])
+        schedlist.append(['8:10PM', '9:55PM', '<em>The Hacker Rant To End All Hacker Rants</em>', 'NCommander', 'Talk', 'In this talk, we have given nearly two hours for our long time recurring speaker to rant about so many technology, community and Information Security topics that would take a novel to list!'])
         schedlist.append(['9:55PM', '10:00PM', 'Wrap-Up', 'Official end of meeting', 'Good night!', '<em>Note: SUBCULTURE closes at 10pm. Please make sure your belongings are together before closing time.'])
-        schedlist.append(['10:00PM', 'zzz', 'Unofficial Ad-Hoc Hours', 'Downtown JC', 'Who\'s up?', 'We may decide to regroup at a local late night venue such as a diner or bar afterwards; this is ad-hoc and entirely unregulated airspace. Participate at your own risk.'])
+        schedlist.append(['10:00PM', 'zzz', 'Unofficial Ad-Hoc Hours', 'Downtown JC', 'Who\'s up?', 'We may decide to regroup at a local late night venue such as a diner or bar afterwards; this is ad-hoc and entirely unregulated airspace. Participate at your own risk! All participants of the justCTF will continue online until December 22nd at 4:00 AM.'])
         for item in schedlist:
             schedtable.append( dict(zip(schedstruct, item)) )
         return schedtable
