2d707dc5f513e1d223a746e2be4bd7e8d6ed2088
Tyler True <944067+sirocyl@users.noreply.github.com>
2020-04-17 00:01:00 -0400

    April online meeting info + A fresh coat of paint
    The schedule page now has better documentation and some decent templates.

--- pages/meetings/__init__.py
+++ pages/meetings/__init__.py
@@ -1,32 +1,137 @@
 #!/usr/bin/env python3
 
-class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend against it for semantics sake.
+class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend against it for semantics sake EXCEPT where triple quotes (""" like this """) appear.
     def Date():			# Date of the meeting (Weekday, Month DD)
         return "Friday, April 17th"
     def Title():		# Title of the meeting
         return "Compile Farm"
     def Location():		# Location of the meeting (as of spring 2019, "SUBCULTURE Jersey City, 260 Newark Ave.")
-        return "ONLINE [SEE LINK]"
+        return "ONLINE [SEE LINK BELOW]"
     def Notice():		# Any important notices about the meeting or the information.
         return ""
     def CanonicalLink():	# A link that refers to this meeting
         return "https://medium.com/@defcon201/defcon-201-online-meet-up-april-2020-compile-farm-ad1e07acb6f5?sk=44919e5c5c42f768ef66fe49ab72de4f"
     def LinkName():		# What this link refers to (for instance "post on our Medium", "forum post", "tweet", "Meetup event")
-        return "Medium:"
+        return "Medium"
     def Summary():		# Short, 1-2 sentences about this meeting.
         return """
-        Join DEFCON 201 Online via Zoom or broacasted on Twitch, dLive or youtube on Friday, April 17th starting at 7:00 PM EST [6:30 PM EST PRE-SHOW] to learn about agricultural and environmentally friendly hacking techniques! <h2><a href="https://stats.foldingathome.org/team/241960">Join the DEFCON 201 Folding@Home Team: 241960</a></h2>"""
+        Join DEFCON 201 Online via Zoom or broacasted on Twitch, dLive or youtube on Friday, April 17th starting at 7:00 PM EST [6:30 PM EST PRE-SHOW]
+        to learn about agricultural and environmentally friendly hacking techniques!
+        <h2><a href="https://stats.foldingathome.org/team/241960">Join the DEFCON 201 Folding@Home Team: 241960</a></h2>"""
+
     def Schedule():
-        schedstruct = ['starttime', 'endtime', 'eventtype', 'title', 'speaker', 'summary']
+        schedstruct = [
+            'starttime',	# The scheduled begin time (for example "6:00 PM") or None if no scheduled time (will put "Ending at [time]" if endtime isn't None)
+            'endtime',		# The scheduled end time (same format) or None if no scheduled time (will put "Starting at [time]" if starttime isn't None)
+            'eventtype',	# The type of event (example choices are: "Intro", "Talk", "Workshop", "Project", "Wrap-Up", "<em>TBA</em>", "Unofficial Ad-Hoc Hours" or something flavorful, or None)
+            'title',		# The title of the event in the schedule, or None.
+            'speaker',		# The speaker or the subject of the event.
+            """summary"""]      # A long-form summary of the event.
+
         schedtable = []
         schedlist = []
-        schedlist.append(['6:20PM', '7:00PM', 'PRE-MEETING HACKER EDUTAINMENT', 'Online Que', 'DEFCON 201 Members', 'To give time to make sure everyone is watching our LiveStream and logging into our LiveStream chat, DEFCON 201 will be showing various videos that relates to our meeting theme.'])
-        schedlist.append(['7:00PM', '7:05PM', 'DEFCON 201 Annoucements & Code of Conduct',  'GI Jack & Sidpocket', 'DEFCON 201', 'DEFCON 201 will start with various updates about our activities in early 2020 and an overview of the Code of Conduct linked on our website.'])
-        schedlist.append(['7:05PM', '7:15PM', 'Empowering a New Local Food System in Urban Environments',  'Mary Wetherill & Electra Jarvis', 'Mary and Electra of Green Food Solutions, will discuss how they are empowering a new local food system, and how you can grow food practically anywhere that has access to electricity and water. They will also give a live demo of their home growing Tower.'])
-        schedlist.append(['7:15PM', '7:25PM', 'Printing Green: An Ecological Cryptocurrency Future',  'BitGreen Foundation', 'Workshop', 'In this talk, the BitGreen Foundation will go over their future goals on the BitGreen project to fund energy efficient and tech activism, how their Proof Of Stake mining method differs from other cryptocurrencies and how to set up your own Master Node directly from the BitGreen Wallet to mine BitGreen on any hardware.'])
-        schedlist.append(['7:25PM', '7:30PM', 'About Rural Tech Fund', 'Chris Sanders', 'Charity PSA', 'The Rural Technology Fund (RTF) recognizes the very real “digital divide” between rural and non-rural areas.'])
-        schedlist.append(['7:30PM', '???', 'Open Workshops Projects', 'Drinking + Games', 'Open participation', 'Join us in the Zoom Chat (email us for access) for our Hacker Show & Tell, Jackbox Party Games, Drinking Alcohol and Juice and 1337 converstations while we lose our minds in isolation!'])
-        schedlist.append(['Bonus', 'CTF', 'April 17th - April 19th', 'PladCTF', 'https://discord.gg/PGgPNEF', 'If you are new to Online CTF, we will help you get set up and walk you thorugh some of the challenges: https://plaidctf.com/'])
+
+        '''
+    Copy the following template to add a new event, filling in the spaces where necessary.
+
+        schedlist.append([
+            '12:00AM',
+            '12:00AM',
+            'Event Type',
+            'Event Title',
+            'Event Speaker or Subject',
+            """This is a summary of the event in question.
+            It can span multiple lines"""
+            ])
+
+    The following line adds a spacer to the schedule box:
+        schedlist.append([None, None, None, None, None, None])
+
+    To add a bold-faced general announcement to the schedule, use this template before the first entry:
+        schedlist.append([None, None, None, "Announcement here", None, None])
+
+    To add a notice after a schedule item, use this template after the entry you wish to add a notice to:
+        schedlist.append([None, None, None, None, "Notice here", None])
+
+    To add a footnote after the end of the schedule, use this template after the last entry:
+        schedlist.append([None, None, None, None, None,
+                          """Footnotes and other important information appear here."""])
+
+        '''
+
+
+        schedlist.append([
+            '6:20PM',
+            '7:00PM',
+            'Online Queue',
+            'PRE-MEETING HACKER EDUTAINMENT',
+            'DEFCON 201 Members',
+            """To give time to make sure everyone is watching our LiveStream and logging into our LiveStream chat, DEFCON 201 will be showing various videos that relates to our meeting theme."""
+            ])
+
+        schedlist.append([
+            '7:00PM',
+            '7:05PM',
+            'DEFCON 201',
+            'DEFCON 201 Annoucements & Code of Conduct',
+            'GI Jack & Sidpocket',
+            """DEFCON 201 will start with various updates about our activities in early 2020 and an overview of the Code of Conduct linked on our website."""
+            ])
+
+        schedlist.append([
+            '7:05PM',
+            '7:15PM',
+            'Talk',
+            'Empowering a New Local Food System in Urban Environments',
+            'Mary Wetherill & Electra Jarvis',
+            """Mary and Electra of Green Food Solutions, will discuss how they are empowering a new local food system, and how you can grow food practically anywhere that has access to electricity and water.
+            They will also give a live demo of their home growing Tower."""
+            ])
+
+        schedlist.append([
+            '7:15PM',
+            '7:25PM',
+            'Workshop',
+            'Printing Green: An Ecological Cryptocurrency Future',
+            'BitGreen Foundation',
+            """In this talk, the BitGreen Foundation will go over their future goals on the BitGreen project to fund energy efficient and tech activism,
+            how their Proof Of Stake mining method differs from other cryptocurrencies and how to set up your own Master Node directly from the BitGreen Wallet to mine BitGreen on any hardware."""
+            ])
+
+        schedlist.append([
+            '7:25PM',
+            '7:30PM',
+            'Charity PSA',
+            'About Rural Tech Fund',
+            'Chris Sanders',
+            """The Rural Technology Fund (RTF) recognizes the very real “digital divide” between rural and non-rural areas."""
+            ])
+
+        schedlist.append([
+            '7:30PM',
+            None,
+            'Open Workshops Projects',
+            'Drinking + Games',
+            'Open participation',
+            """Join us in the Zoom Chat (email us for access) for our Hacker Show & Tell, Jackbox Party Games, Drinking Alcohol and Juice and 1337 converstations while we lose our minds in isolation!"""
+            ])
+
+        schedlist.append([None, None, None, None, None, None])
+
+        schedlist.append([
+            'April 17th',
+            'April 19th',
+            'Bonus',
+            'CTF',
+            'PlaidCTF',
+            """<a href="https://discord.gg/PGgPNEF">PlaidCTF on Discord</a><br>
+            If you are new to Online CTF, we will help you get set up and walk you thorugh some of the challenges: https://plaidctf.com/"""
+            ])
+
+        schedlist.append([None, None, None, None, None, None])
+        schedlist.append([None, None, "COVID-19 Notice", "Keep an eye out for your fellow hacker. Whether you're social or anti-social, practice distancing.", None, None])
+
+
         for item in schedlist:
             schedtable.append( dict(zip(schedstruct, item)) )
         return schedtable
@@ -34,9 +139,44 @@ class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend a
     def FormattedSchedule():
         schedule = MeetingDetails.Schedule()
         html = ""
+
         for item in schedule:
-            html += "<h5>" + item["starttime"] + " - " + item["endtime"] + ": <em>" + item["eventtype"] + ": " + item["title"] + "</em> <span style=\"font-weight: 300;\">&nbsp;| " + item["speaker"] + "</span></h5>\n"
-            html += "<p>" + item["summary"] + "</p>\n"
+
+            html += "<h5>"
+
+            if item["starttime"] is not None or item["endtime"] is not None:
+                if item["starttime"] is not None:
+                    if item["endtime"] is None:
+                        html += "Starting at "
+                    html += item["starttime"]
+                if item["starttime"] is not None and item["endtime"] is not None:
+                    html += " - "
+                if item["endtime"] is not None:
+                    if item["starttime"] is None:
+                        html += "Ending at "
+                    html += item["endtime"]
+                html += ": "
+
+            html += "<em>"
+
+            if item["eventtype"] is not None:
+                html += item["eventtype"] + ": "
+
+            if item["title"] is not None:
+                html += item["title"]
+
+            html += "</em>"
+
+            if item["speaker"] is not None:
+                html += " <span style=\"font-weight: 300;\">&nbsp;| " + item["speaker"] + "</span>"
+
+            html += "</h5>\n"
+
+            if item["summary"] is not None:
+                html += "<p>" + item["summary"] + "</p>\n"
+            else:
+                html += "<br>"
+
         return html
 
 def main():
@@ -56,4 +196,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
