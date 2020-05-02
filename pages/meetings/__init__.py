07b658644be72150dcef6fe5d8c5b1e72a21f6fb
Tyler True <944067+sirocyl@users.noreply.github.com>
2020-05-02 18:15:55 -0400

    Add preliminary May meeting info.

--- pages/meetings/__init__.py
+++ pages/meetings/__init__.py
@@ -2,22 +2,21 @@
 
 class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend against it for semantics sake EXCEPT where triple quotes (""" like this """) appear.
     def Date():			# Date of the meeting (Weekday, Month DD)
-        return "Friday, April 17th"
+        return "Friday, May 15th"
     def Title():		# Title of the meeting
-        return "Compile Farm"
+        return "Mind Games"
     def Location():		# Location of the meeting (as of spring 2019, "SUBCULTURE Jersey City, 260 Newark Ave.")
-        return "ONLINE [SEE LINK BELOW]"
+        return "Online &mdash; Streaming Live"
     def Notice():		# Any important notices about the meeting or the information.
         return ""
     def CanonicalLink():	# A link that refers to this meeting
-        return "https://medium.com/@defcon201/defcon-201-online-meet-up-april-2020-compile-farm-ad1e07acb6f5?sk=44919e5c5c42f768ef66fe49ab72de4f"
+        return "https://medium.com/@defcon201/"
     def LinkName():		# What this link refers to (for instance "post on our Medium", "forum post", "tweet", "Meetup event")
         return "Medium"
     def Summary():		# Short, 1-2 sentences about this meeting.
         return """
-        Join DEFCON 201 Online via Zoom or broacasted on Twitch, dLive or youtube on Friday, April 17th starting at 7:00 PM EST [6:30 PM EST PRE-SHOW]
-        to learn about agricultural and environmentally friendly hacking techniques!
-        <h2><a href="https://stats.foldingathome.org/team/241960">Join the DEFCON 201 Folding@Home Team: 241960</a></h2>"""
+        More information about this month's meeting is coming soon.
+        """
 
     def Schedule():
         schedstruct = [
@@ -61,72 +60,15 @@ class MeetingDetails:		# Any of these can be HTML formatted, but I'd recommend a
 
 
         schedlist.append([
-            '6:20PM',
             '7:00PM',
-            'Online Queue',
-            'PRE-MEETING HACKER EDUTAINMENT',
-            'DEFCON 201 Members',
-            """To give time to make sure everyone is watching our LiveStream and logging into our LiveStream chat, DEFCON 201 will be showing various videos that relates to our meeting theme."""
-            ])
-
-        schedlist.append([
-            '7:00PM',
-            '7:05PM',
-            'DEFCON 201',
-            'DEFCON 201 Annoucements & Code of Conduct',
-            'GI Jack & Sidpocket',
-            """DEFCON 201 will start with various updates about our activities in early 2020 and an overview of the Code of Conduct linked on our website."""
-            ])
-
-        schedlist.append([
-            '7:05PM',
-            '7:15PM',
-            'Talk',
-            'Empowering a New Local Food System in Urban Environments',
-            'Mary Wetherill & Electra Jarvis',
-            """Mary and Electra of Green Food Solutions, will discuss how they are empowering a new local food system, and how you can grow food practically anywhere that has access to electricity and water.
-            They will also give a live demo of their home growing Tower."""
-            ])
-
-        schedlist.append([
-            '7:15PM',
-            '7:25PM',
-            'Workshop',
-            'Printing Green: An Ecological Cryptocurrency Future',
-            'BitGreen Foundation',
-            """In this talk, the BitGreen Foundation will go over their future goals on the BitGreen project to fund energy efficient and tech activism,
-            how their Proof Of Stake mining method differs from other cryptocurrencies and how to set up your own Master Node directly from the BitGreen Wallet to mine BitGreen on any hardware."""
-            ])
-
-        schedlist.append([
-            '7:25PM',
-            '7:30PM',
-            'Charity PSA',
-            'About Rural Tech Fund',
-            'Chris Sanders',
-            """The Rural Technology Fund (RTF) recognizes the very real “digital divide” between rural and non-rural areas."""
-            ])
-
-        schedlist.append([
-            '7:30PM',
+            '9:00PM',
             None,
-            'Open Workshops Projects',
-            'Drinking + Games',
-            'Open participation',
-            """Join us in the Zoom Chat (email us for access) for our Hacker Show & Tell, Jackbox Party Games, Drinking Alcohol and Juice and 1337 converstations while we lose our minds in isolation!"""
+            'DEFCON 201 Meeting',
+            None,
+            None
             ])
 
-        schedlist.append([None, None, None, None, None, None])
-
-        schedlist.append([
-            'April 17th',
-            'April 19th',
-            'Bonus',
-            'CTF',
-            'PlaidCTF',
-            """<a href="https://discord.gg/PGgPNEF">PlaidCTF on Discord</a><br>
-            If you are new to Online CTF, we will help you get set up and walk you thorugh some of the challenges: https://plaidctf.com/"""
-            ])
+        schedlist.append([None, None, None, None, "Schedule, speakers and participants are to be determined.", None])
 
         schedlist.append([None, None, None, None, None, None])
         schedlist.append([None, None, "COVID-19 Notice", "Keep an eye out for your fellow hacker. Whether you're social or anti-social, practice distancing.", None, None])
